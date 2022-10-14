176dc45eb7bcc7930baf290e3adb513212a78a0a
indradian <88955146+indradian@users.noreply.github.com>
2022-10-14 18:04:06 +0700

    initial commit

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
-var savedItem = localStorage.getItem("savedItem");
-function change(){
+var savedItemName = localStorage.getItem("savedItemName");
+var savedItemEmail = localStorage.getItem("savedItemEmail");
+function changeName(){
   var name = prompt("Type your name");
-    localStorage.setItem("savedItem", name);
-    location.reload()
-  
+    localStorage.setItem("savedItemName", name);
+    location.reload()  
+}
+function changeEmail(){
+  var email = prompt("Type your e-mail");
+    localStorage.setItem("savedItemEmail", email);
+    location.reload()  
 }
 function getProfile(){
-  localStorage.getItem("savedItem");
-  document.getElementById("profile-txt").innerHTML = savedItem + "'s Profile";
-  document.getElementById("saved-name").innerHTML = savedItem;
+  localStorage.getItem("savedItemName");
+  localStorage.getItem("savedItemEmail");
+  document.getElementById("profile-txt").innerHTML = savedItemName + "'s Profile";
+  document.getElementById("saved-name").innerHTML = ": " + savedItemName;
+  document.getElementById("saved-email").innerHTML = ": " + savedItemEmail;
 
 }
 function getIndex(){
-  localStorage.getItem("savedItem");
-  document.getElementById("profile-txt").innerHTML = savedItem + "'s Profile";
-  document.getElementById("saved-welcome").innerHTML = "Hello, " + savedItem;
+  localStorage.getItem("savedItemName");
+  document.getElementById("profile-txt").innerHTML = savedItemName + "'s Profile";
+  document.getElementById("welcome1").innerHTML = "Hello, " + savedItemName;
+}
+//Header
+const header = document.querySelector('.navigation');
+window.onscroll = function(){
+  var top = window.scrollY;
+  console.log(top);
+  if(top >= 650){
+    header.classList.add('active')
+  }else{
+    header.classList.remove('active');
+  }
 }
